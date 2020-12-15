a48752d4d7b6c50279f1250c0b12b79e43180eb3
TOM WALLYN <tom.wallyn@epsi.fr>
2020-12-15 18:07:35 +0100

    Ajout des roles

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -18,8 +18,6 @@ class RegistrationFormType extends AbstractType
     {
         $builder
             ->add('UserName')
-            ->add('nom')
-            ->add('prenom')
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
