4a8eba1f41b778b9dbc161f12273fdd56845e79a
yoann collot <collotyoann@homtail.fr>
2020-12-15 14:37:17 +0100

    mofif form register, base.html.twig,

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -17,15 +17,10 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+            ->add('nom')
+            ->add('prenom')
             ->add('UserName')
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
-                    ]),
-                ],
-            ])
+
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
