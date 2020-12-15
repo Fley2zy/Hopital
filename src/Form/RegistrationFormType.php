65a0b7308ab7a7155fc57349adcd7f99d2b39fdf
TOM WALLYN <tom.wallyn@epsi.fr>
2020-12-15 14:37:07 +0100

    Modification de utilisateur et des formulaire, ajout des différentes route

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -18,14 +18,8 @@ class RegistrationFormType extends AbstractType
     {
         $builder
             ->add('UserName')
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
-                    ]),
-                ],
-            ])
+            ->add('nom')
+            ->add('prenom')
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
